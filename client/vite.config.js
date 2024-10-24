40e37d14ae77f3a4c993dbf86a752e8a74fee76a
abbas-ali-syed <21ucc004@lnmiit.ac.in>
2024-10-24 23:57:16 +0530

    seventh

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-     '/api': 'http://localhost:8804',
+     '/api': 'https://lnmiit-bus-app.onrender.com',
     },
   },
 
